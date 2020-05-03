85dbb5165b5bb8aa74d3c262315fce4315a78535
12916 <12916@10.0.0.156>
2020-05-02 20:00:21 -0700

    Alex

---
+++ Group3/src/DB/DB.java
@@ -0,0 +1,124 @@
+package DB;
+
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author Alex, Sjie
+ */
+public class DB{
+    String className = "jdbc:derby://localhost:1527/VirusData";
+    String url = "jdbc:derby://localhost:1527/VirusData";
+    String user = "APP";
+    String password = "APP";
+    String date;
+    String country;
+    
+    public String extractData(int choice, String date, String country){
+        switch(choice){
+            case 1://determine the total death by date
+                String TotalRecovered = "";
+                try{
+                    Connection myConn = DriverManager.getConnection(url,user,password);  
+                    Statement stmt = myConn.createStatement();
+                    String sql = "SELECT DEATHS FROM CORONAVIRUSWORLD WHERE DATE = '" + date + "'";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    while(rs.next())TotalRecovered = String.valueOf(rs.getString(1));
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return TotalRecovered;
+                
+            case 2://determine current total confirmed cases per day
+                String TotalCase = "";
+                try{
+                    Connection myConn = DriverManager.getConnection(url,user,password);  
+                    Statement stmt = myConn.createStatement();
+                    String sql = "SELECT CONFIRMED FROM CORONAVIRUSWORLD WHERE DATE = '" + date + "'";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    while(rs.next())TotalCase = String.valueOf(rs.getString(2));
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return TotalCase;
+                
+            case 3://determine new increased confirmed cases per day
+                String IncreasedCase = "";
+                try{
+                    Connection myConn = DriverManager.getConnection(url,user,password);  
+                    Statement stmt = myConn.createStatement();
+                    String sql = "SELECT CONFIRMED FROM CORONAVIRUSWORLD WHERE DATE = '" + date + "'";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    while(rs.next())IncreasedCase = String.valueOf(rs.getString(2));//need calculation
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return IncreasedCase;
+                
+            case 4://determine total reported death per day
+                String TotalDeaths = "";
+                try{
+                    Connection myConn = DriverManager.getConnection(url,user,password);  
+                    Statement stmt = myConn.createStatement();
+                    String sql = "SELECT DEATHS FROM CORONAVIRUSWORLD WHERE DATE = '" + date + "'";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    while(rs.next())TotalDeaths = String.valueOf(rs.getString(2));
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return TotalDeaths;
+                
+            case 5://determine total cases in each country
+                String CountryCase = "";
+                try{
+                    Connection myConn = DriverManager.getConnection(url,user,password);  
+                    Statement stmt = myConn.createStatement();
+                    String sql = "SELECT CONFIRMED FROM CORONAVIRUSCOUNTRIES WHERE COUNTRY = '" + country + "'";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    while(rs.next())CountryCase = String.valueOf(rs.getString(2));
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return CountryCase;
+               
+            case 6://determine total recovered cases in each country
+                String CountryRecovered = "";
+                try{
+                    Connection myConn = DriverManager.getConnection(url,user,password);  
+                    Statement stmt = myConn.createStatement();
+                    String sql = "SELECT RECOVERED FROM CORONAVIRUSCOUNTRIES WHERE COUNTRY = '" + country + "'";
+                    ResultSet rs = stmt.executeQuery(sql);
+                    while(rs.next())CountryRecovered = String.valueOf(rs.getString(2));
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return CountryRecovered;
+                
+            default:
+                return null;
+        }
+    }
+    
+    public void setDate(String date){
+        this.date = date;
+    }
+    
+    public String getDate(){
+        return this.date;
+    }
+
+    public void setCountry(String country){
+        this.country = country;
+    }
+    
+    public String getCountry(){
+        return this.country;
+    }
+}
---
+++ Group3/src/JFrame.java
@@ -0,0 +1,171 @@
+import DB.DB;
+
+public class JFrame extends javax.swing.JFrame {
+    
+    DB myObject = new DB();
+    
+    public JFrame() {
+        initComponents();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        TFDate = new javax.swing.JTextField();
+        TFCountry = new javax.swing.JTextField();
+        LBDate = new javax.swing.JLabel();
+        LBCountry = new javax.swing.JLabel();
+        CB = new javax.swing.JComboBox<>();
+        LBDisplay = new javax.swing.JLabel();
+        LBOutput = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        TFDate.setFont(new java.awt.Font("Tahoma", 0, 20)); // NOI18N
+
+        TFCountry.setFont(new java.awt.Font("Tahoma", 0, 20)); // NOI18N
+        TFCountry.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TFCountryActionPerformed(evt);
+            }
+        });
+
+        LBDate.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        LBDate.setText("Date");
+
+        LBCountry.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        LBCountry.setText("Country");
+
+        CB.setFont(new java.awt.Font("Tahoma", 0, 20)); // NOI18N
+        CB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "total death by date", "current total confirmed cases per day", "new increased confirmed cases per day", "total reported death per day", "total cases in each country", "total recovered cases in each country" }));
+
+        LBDisplay.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        LBDisplay.setText("Result:");
+
+        LBOutput.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        LBOutput.setText(" ");
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jButton1.setText("Submit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(CB, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(LBDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(54, 54, 54)
+                                .addComponent(LBOutput, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(LBDate)
+                                .addGap(18, 18, 18)
+                                .addComponent(TFDate, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(LBCountry)
+                                .addGap(18, 18, 18)
+                                .addComponent(TFCountry, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(188, 188, 188)
+                        .addComponent(jButton1)))
+                .addContainerGap(42, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LBDate)
+                    .addComponent(TFCountry, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LBCountry)
+                    .addComponent(TFDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(35, 35, 35)
+                .addComponent(CB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LBDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LBOutput, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(36, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void TFCountryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TFCountryActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TFCountryActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String date;
+        String country;
+        date = TFDate.getText();
+        country = TFCountry.getText();
+        
+        myObject.setDate(date);
+        myObject.setCountry(country);        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new JFrame().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> CB;
+    private javax.swing.JLabel LBCountry;
+    private javax.swing.JLabel LBDate;
+    private javax.swing.JLabel LBDisplay;
+    private javax.swing.JLabel LBOutput;
+    private javax.swing.JTextField TFCountry;
+    private javax.swing.JTextField TFDate;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    // End of variables declaration//GEN-END:variables
+}
